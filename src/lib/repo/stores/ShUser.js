16382846a5d2842644a7c35e67dde8f342abf38b
Ian Kibet <kibet@hauzisha.co.ke>
2024-02-26 11:56:14 +0300

    wip permissions

--- src/lib/repo/stores/ShUser.js
+++ src/lib/repo/stores/ShUser.js
@@ -56,7 +56,7 @@ export const useUserStore = defineStore('user-store', {
             } else {
               permissions = this.permissions
             }
-            return permissions.includes(slug)
+            return !!permissions[slug]
           }
           return false
         }
