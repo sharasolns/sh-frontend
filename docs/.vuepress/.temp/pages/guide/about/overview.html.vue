023a3b4a5fee6ad23f81aee1e24d1c231cbcfb0c
silah <silakosy@gmail.com>
2024-03-04 10:27:35 +0300

    update documentation overview

--- docs/.vuepress/.temp/pages/guide/about/overview.html.vue
+++ docs/.vuepress/.temp/pages/guide/about/overview.html.vue
@@ -1,8 +1,18 @@
 <template><div><h1 id="overview" tabindex="-1"><a class="header-anchor" href="#overview"><span>Overview</span></a></h1>
-<h1 id="" tabindex="-1"><a class="header-anchor" href="#"><span>==============</span></a></h1>
-<h1 id="this-is-the-overview-of-the-project" tabindex="-1"><a class="header-anchor" href="#this-is-the-overview-of-the-project"><span>This is the overview of the project.</span></a></h1>
-<h1 id="the-project-is-a-collection-of-components-and-utilities-that-can-be-used-to-build-web-applications" tabindex="-1"><a class="header-anchor" href="#the-project-is-a-collection-of-components-and-utilities-that-can-be-used-to-build-web-applications"><span>The project is a collection of components and utilities that can be used to build web applications.</span></a></h1>
-<h1 id="the-project-is-built-using-vue-js-and-is-designed-to-be-used-with-vue-js-applications" tabindex="-1"><a class="header-anchor" href="#the-project-is-built-using-vue-js-and-is-designed-to-be-used-with-vue-js-applications"><span>The project is built using Vue.js and is designed to be used with Vue.js applications.</span></a></h1>
+<p>This project was inspired by the need to have a collection of components and utilities that can be used to build web applications.
+While working on different projects, we found ourselves repeating the same code over and over again Such as form components, sweet alerts, and other utilities. This was time-consuming and not efficient.
+We decided to create a collection of components and utilities that I can use to build web applications. This is how shFramework was born.</p>
+<p>After using it for a while, we decided to share it with the community. We believe that it will help other developers to build web applications faster and more efficiently.
+Also to help in the development of the package, we have created a <RouteLink to="/guide/contribution.html">contribution guide</RouteLink> to help in the development of the package.
+We are open to contributions and suggestions to make the package better.</p>
+<p>Let's build a better web application together.</p>
+<h2 id="features" tabindex="-1"><a class="header-anchor" href="#features"><span>Features</span></a></h2>
+<ul>
+<li><strong>Bootstrap 5</strong>: shFramework uses Bootstrap 5 for styling. This makes it easy to create stunning web applications.</li>
+<li><strong>Vue 3</strong>: shFramework is designed to be used with Vue 3 applications.</li>
+<li><strong>Axios</strong>: shFramework uses axios to make requests to the backend.</li>
+<li><strong>Sweet Alerts</strong>: shFramework uses sweet alerts to display alerts to the user.</li>
+</ul>
 </div></template>
 
 
--- docs/guide/about/overview.md
+++ docs/guide/about/overview.md
@@ -1,9 +1,19 @@
 # Overview
-# ==============
 
-# This is the overview of the project.
+This project was inspired by the need to have a collection of components and utilities that can be used to build web applications.
+While working on different projects, we found ourselves repeating the same code over and over again Such as form components, sweet alerts, and other utilities. This was time-consuming and not efficient.
+We decided to create a collection of components and utilities that I can use to build web applications. This is how shFramework was born.
 
-# The project is a collection of components and utilities that can be used to build web applications.
+After using it for a while, we decided to share it with the community. We believe that it will help other developers to build web applications faster and more efficiently.
+Also to help in the development of the package, we have created a [contribution guide](/guide/contribution.md) to help in the development of the package. 
+We are open to contributions and suggestions to make the package better.
 
-# The project is built using Vue.js and is designed to be used with Vue.js applications.
+Let's build a better web application together.
+
+## Features
+
+- **Bootstrap 5**: shFramework uses Bootstrap 5 for styling. This makes it easy to create stunning web applications.
+- **Vue 3**: shFramework is designed to be used with Vue 3 applications.
+- **Axios**: shFramework uses axios to make requests to the backend. 
+- **Sweet Alerts**: shFramework uses sweet alerts to display alerts to the user.
 
