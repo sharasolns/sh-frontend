7aa48fb65e79ae8be36c234ae26545955f9b88f7
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2024-10-06 10:59:26 +0300

    wip key binding

--- src/views/core/notes/FormatTitle.vue
+++ src/views/core/notes/FormatTitle.vue
@@ -1,12 +1,12 @@
 <script setup>
-const props = defineProps(['item','column'])
+const props = defineProps(['item','label'])
 const emits = defineEmits(['rowSelected'])
 
 
 </script>
 
 <template>
-  <h3><input @click="emits('rowSelected',item)" type="checkbox"/> {{ column }}</h3>
+  <h3><input @click="emits('rowSelected',item)" type="checkbox"/> {{ label }}</h3>
 </template>
 
 <style scoped>
