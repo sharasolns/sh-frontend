2637aa70bafde80d25061c686431330bc72d4e37
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2024-10-06 10:13:36 +0300

    attached column to table view component

--- src/views/core/notes/FormatTitle.vue
+++ src/views/core/notes/FormatTitle.vue
@@ -1,12 +1,12 @@
 <script setup>
-const props = defineProps(['item'])
+const props = defineProps(['item','column'])
 const emits = defineEmits(['rowSelected'])
 
 
 </script>
 
 <template>
-  <h3><input @click="emits('rowSelected',item)" type="checkbox"/></h3>
+  <h3><input @click="emits('rowSelected',item)" type="checkbox"/> {{ column }}</h3>
 </template>
 
 <style scoped>
