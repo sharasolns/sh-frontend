c3895ef4b0a49a792b72c04a8aa71eb7f26de5aa
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2025-09-17 13:51:30 +0300

    wip tab count issues

--- src/views/core/tabs/TabsMain.vue
+++ src/views/core/tabs/TabsMain.vue
@@ -1,14 +1,18 @@
 <script setup>
 
 import ShTabs from '@/lib/components/ShTabs.vue'
+import { ref } from 'vue'
+const tabCounts = ref({
+  // active_tasks: 133
+})
 </script>
 
 <template>
-<sh-tabs :tabs="[
+<sh-tabs :tab-counts="tabCounts" :tabs="[
     {
      key: 'active_tasks',
-     permission: 'tasks.active',
-     label: 'All Active Tasks'
+     label: 'All Active Tasks',
+     count: 12
     }
 ]"
          :base-url="`/tabs`"/>
