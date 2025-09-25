fde11ba45673f13287ab1abefc660c325454a463
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2025-09-25 14:16:08 +0300

    wip tab counts

--- src/lib/components/ShTabs.vue
+++ src/lib/components/ShTabs.vue
@@ -5,7 +5,6 @@ import { useRoute, useRouter } from 'vue-router'
 import shRepo from '../repo/helpers/ShRepo.js'
 import { storeToRefs } from 'pinia'
 import { useUserStore } from '../repo/stores/ShUser'
-import { all } from 'axios'
 
 const props = defineProps({
   tabs: {
@@ -44,16 +43,27 @@ const currentTab = ref('')
 const path = ref(route.path)
 const allowedTabs = ref([])
 const {user} = storeToRefs(useUserStore())
+const realTabCounts = ref({})
 onMounted(()=>{
   if(props.tabs.length === 0){
     return;
   }
   allowedTabs.value = props.tabs.filter(tab=>{
-    if(typeof tab === 'object' && tab.permission){
-      if(user.value && user.value.isAllowedTo(tab.permission)){
-        return tab
+    if(typeof tab === 'object'){
+      if(tab.count || tab.counts){
+        realTabCounts.value[tab.name || tab.key] = tab.count || tab.counts
       }
-      return false
+      if(tab.validator){
+        if(tab.validator()){
+          return tab
+        }
+        return false
+      } else if(tab.permission){
+        if(user.value && user.value.isAllowedTo(tab.permission)){
+          return tab
+        }
+        return false
+      }
     }
     return tab
   })
@@ -63,9 +73,12 @@ onMounted(()=>{
   }
 })
 
-watch(()=>props.tabCounts, () => {
+watch(()=>props, () => {
+  console.log('changed')
   resetTabCounts()
-})
+},{
+  deep: true}
+)
 
 watch(()=>route.path,()=>{
   const arr = route.fullPath.split('/')
@@ -87,14 +100,7 @@ const setTab = (tab) => {
   }
 }
 const setTabCounts = (tabCounts) => {
-  console.log('Setting tab counts', tabCounts)
-  if (typeof tabCounts === 'object') {
-    setCounts(tabCounts)
-  } else {
-    apis.doGet(tabCounts).then(res => {
-      setCounts(res.data)
-    })
-  }
+
 }
 const resetTabCounts = () => {
   const arr = route.fullPath.split('/')
@@ -104,12 +110,16 @@ const resetTabCounts = () => {
     setTab(arr[arr.length - 1])
   }
   let tbCounts = props.tabCounts
-  if(!tbCounts && typeof props.tabs[0] == 'object'){
-    tbCounts = props.tabs
+  // merge with realTabCounts
+  if (tbCounts && typeof tbCounts === 'object') {
+    realTabCounts.value = {...realTabCounts.value, ...tbCounts}
+  } else if(typeof tbCounts === 'string') {
+    // it is a string fetch from api
+    apis.doGet(tbCounts).then(res => {
+      realTabCounts.value = {...realTabCounts.value, ...res.data}
+    })
   }
-  if (props.tabCounts) {
-    setTabCounts(tbCounts)
-  }
+
 }
 const tabExistsInUrl = () => {
   let exists = false
@@ -120,20 +130,6 @@ const tabExistsInUrl = () => {
     }
   })
   return exists
-}
-const setCounts = (res) => {
-  Object.keys(res).forEach(key => {
-    console.log('Setting count for tab', key, res[key])
-    let elem = document.getElementsByClassName('sh_tab_' + key)
-    if (elem.length > 0) {
-      elem = elem[0]
-      let txt = elem.innerHTML
-      txt = txt.split('<i class="d-none"></i>')[0]
-      if (parseInt(res[key]) > 0) {
-        elem.innerHTML = txt + '<i class="d-none"></i><sup class="sh_tab_count">' + res[key] + '</sup>'
-      }
-    }
-  })
 }
 
 const getTabKey = (tab)=>{
@@ -161,12 +157,16 @@ const getTabLabel = tab=>{
 }
 </script>
 <template>
+  {{ currentTab }}
   <template v-if="allowedTabs.length > 0">
     <ul class="nav nav-tabs sh-tabs" :class="classes ?? shRepo.getShConfig('tabsClass','sh-tabs nav-tabs-bordered')">
       <li class="nav-item" v-for="tab in allowedTabs" :key="getTabKey(tab)" v-if-user-can="getTabPermission(tab)">
         <router-link @click="setTab(tab)" :active-class="'active'" class="nav-link text-capitalize"
                      :to="baseUrl+'/tab/'+getTabKey(tab)" role="tab" :class="'sh_tab_' + getTabKey(tab)">
           {{ getTabLabel(tab) }}
+          <sup class="sh_tab_count" v-if="realTabCounts[getTabKey(tab)] !== undefined && realTabCounts[getTabKey(tab)] > 0">
+            ({{ realTabCounts[getTabKey(tab)] }})
+          </sup>
         </router-link>
       </li>
     </ul>
--- src/views/core/tabs/TabsMain.vue
+++ src/views/core/tabs/TabsMain.vue
@@ -1,14 +1,24 @@
 <script setup>
 
 import ShTabs from '@/lib/components/ShTabs.vue'
+import { ref } from 'vue'
+
+const tabCounts = ref()
+
+setTimeout(()=>{
+  tabCounts.value = {
+    active_tasks: 25
+  }
+},2000)
 </script>
 
 <template>
-<sh-tabs :tabs="[
+<sh-tabs :tab-counts="tabCounts" :tabs="[
     {
      key: 'active_tasks',
      label: 'All Active Tasks',
-     count: 12
+     count: 12,
+     validator: ()=>true
     }
 ]"
          :base-url="`/tabs`"/>
